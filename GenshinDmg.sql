a27d4bb33414fafd0c4ea970b8f75ea42d49cf51
VXYtmw <ruicheng_ao@foxmail.com>
2024-06-16 19:50:44 +0800

    Sql for table built added. Postman json file modified.

---
+++ GenshinDmg.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE genshin_dmg;
+USE genshin_dmg;
+
+CREATE TABLE user (
+    user_id INT(32) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(32) NOT NULL UNIQUE,
+    user_password CHAR(64) NOT NULL,
+    mail VARCHAR(320) NOT NULL UNIQUE,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE artifact (
+    artifact_id INT(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(32) UNSIGNED,
+    suit INT(16) UNSIGNED NOT NULL,
+    part INT(4) UNSIGNED NOT NULL,
+    primary_attribute INT(8) UNSIGNED NOT NULL,
+    primary_value FLOAT NOT NULL,
+    secondary_attribute1 INT(8) UNSIGNED NOT NULL,
+    secondary_attribute2 INT(8) UNSIGNED NOT NULL,
+    secondary_attribute3 INT(8) UNSIGNED NOT NULL,
+    secondary_attribute4 INT(8) UNSIGNED NOT NULL,
+    secondary_value1 FLOAT NOT NULL,
+    secondary_value2 FLOAT NOT NULL,
+    secondary_value3 FLOAT NOT NULL,
+    secondary_value4 FLOAT NOT NULL,
+    CONSTRAINT fk_artifact_user
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    CHECK (part >= 1 AND part <=5)
+);
+
+INSERT INTO user(user_name, user_password, mail) VALUES('arc', 'A665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3', 'ruicheng_ao@foxmail.com');  # password is '123'
+INSERT INTO artifact(user_id, suit, part, primary_attribute, primary_value, secondary_attribute1, secondary_value1, secondary_attribute2, secondary_value2, secondary_attribute3, secondary_value3, secondary_attribute4, secondary_value4) VALUES (1, 5, 1, 4, 4780, 3, 0.198, 7, 37,  9, 0.074, 10, 0.054);
